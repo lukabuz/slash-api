d06f6223c890dba6a676f67f2a9d2421223a2e94
lukabuz <buzaladzeluka@gmail.com>
2019-10-20 16:53:57 -0400

    added comments

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,7 @@ import * as cors from "cors";
 import * as parser from "body-parser";
 // import { app } from "firebase-admin";
 
+// function for determining if a body is a json string
 const isJsonString = (str: any): boolean => {
 	try {
 		JSON.parse(str);
@@ -14,6 +15,7 @@ const isJsonString = (str: any): boolean => {
 	return true;
 };
 
+// middleware for parsing req.body
 const parsingMiddleware = (req: any, res: any, next: any) => {
 	if (isJsonString(req.body)) {
 		req.body = JSON.parse(req.body);
@@ -21,44 +23,48 @@ const parsingMiddleware = (req: any, res: any, next: any) => {
 	next();
 };
 
+// interface for validator configuration
 interface validationConfig {
-	variableName: string;
-	displayName: string;
-	minLength: number;
-	maxLength: number;
+	variableName: string; // name of variable in http request
+	displayName: string; // what the variable should be reffered to as in errors
+	minLength: number; // minimum length of the value
+	maxLength: number; // maximum length of the value
 }
 
+// Validator class, handles http request body validation
 class Validator {
-	private configs: validationConfig[];
-	private body: any;
-	public errors: string[];
+	private configs: validationConfig[]; // array for configs of each expected variable
+	private body: any; // req.body
+	public errors: string[]; // array of errors found in req.body
 
+	// constructor, takes an array of configs and a req.body
 	constructor(configs: validationConfig[], body: any) {
 		this.configs = configs;
 		this.errors = [];
 		this.body = body;
 	}
 
+	// the validate method. First checks if all variables ar present, then checks their size
+	// if this returns false, this.errors.length > 0
 	public validate(): boolean {
-		if (this.checkExistence()) {
-			return this.checkLength();
-		} else {
-			return false;
-		}
+		return this.checkExistence() && this.checkLength();
 	}
 
+	// check if all variables in the config exist
 	private checkExistence(): boolean {
 		for (const config of this.configs) {
 			if (typeof this.body[config.variableName] === "undefined") {
-				this.errors.push("გთხოვთ შეიყვანოთ " + config.displayName);
+				this.errors.push("გთხოვთ შეიყვანოთ " + config.displayName); // push to errors array
 			}
 		}
 		return this.errors.length === 0;
 	}
 
+	// check lengths of all variables
 	private checkLength(): boolean {
 		for (const config of this.configs) {
 			if (this.body[config.variableName].length > config.maxLength) {
+				// check for max
 				this.errors.push(
 					config.displayName +
 						" უნდა იყოს მაქსიმუმ " +
@@ -67,6 +73,7 @@ class Validator {
 				);
 			}
 			if (this.body[config.variableName].length < config.minLength) {
+				// check for min
 				this.errors.push(
 					config.displayName +
 						" უნდა იყოს მინიმუმ " +
@@ -79,10 +86,12 @@ class Validator {
 	}
 }
 
+// Mailer class, handles email sending
 class Mailer {
-	private transporter: any;
-	private user: string;
+	private transporter: any; // nodemailer transporter object
+	private user: string; // username(example@example.org) of sending account
 
+	// constructor, requires mail account info from config
 	constructor(
 		host: string,
 		port: number,
@@ -93,6 +102,7 @@ class Mailer {
 		this.user = user;
 
 		this.transporter = nodemailer.createTransport({
+			// create transporter and save it
 			host: host,
 			port: port,
 			secure: secure,
@@ -103,15 +113,16 @@ class Mailer {
 		});
 	}
 
+	// sending function. resolves or rejects with a string return value
 	async send(to: string, body: string, subject: string) {
 		return new Promise((resolve: any, reject: any) => {
 			this.transporter
 				.sendMail({
-					from: '"Slash" <' + this.user + ">", // sender address
-					to: to, // list of receivers
-					subject: subject, // Subject line
-					text: body, // plain text body
-					html: body // html body
+					from: '"Slash" <' + this.user + ">", // from
+					to: to, // to
+					subject: subject, // subject
+					text: body, // plaintext email
+					html: body // html email
 				})
 				.then((res: any) => {
 					console.log(res);
@@ -125,22 +136,25 @@ class Mailer {
 	}
 }
 
+// initialize express, set cors, and handle body parsing
 const api = express();
 api.use(cors({ origin: true }));
 api.use(parser.urlencoded({ extended: false }));
 api.use(parser.json());
 api.use(parsingMiddleware);
 
+// email account configuration from firebase
 const mailConfig = {
 	host: functions.config().email.host,
-	port: +functions.config().email.port,
-	secure: +functions.config().email.port === 465,
+	port: +functions.config().email.port, //typecast to number
+	secure: +functions.config().email.port === 465, // only true if port is 465
 	auth: {
 		user: functions.config().email.auth,
 		pass: functions.config().email.pass
 	}
 };
 
+//initialize mailer
 const mailer = new Mailer(
 	mailConfig.host,
 	mailConfig.port,
@@ -149,7 +163,9 @@ const mailer = new Mailer(
 	mailConfig.auth.pass
 );
 
+// route for requesting a price quote from the website
 api.post("/requestQuote", async (req: any, res: any) => {
+	// initialize validator with the proper configs
 	const validator = new Validator(
 		[
 			{
@@ -168,11 +184,13 @@ api.post("/requestQuote", async (req: any, res: any) => {
 		req.body
 	);
 
+	// handle validator errors
 	if (!validator.validate()) {
 		res.json({ status: "error", errors: validator.errors });
 		return;
 	}
 
+	// prepare payload and send email
 	const text = "From: " + req.body.email + "\n" + req.body.text;
 
 	mailer
@@ -185,7 +203,9 @@ api.post("/requestQuote", async (req: any, res: any) => {
 		});
 });
 
+// route for handling the contact form
 api.post("/contact", async (req: any, res: any) => {
+	// initialize validator with the proper configs
 	const validator = new Validator(
 		[
 			{
@@ -204,11 +224,13 @@ api.post("/contact", async (req: any, res: any) => {
 		req.body
 	);
 
+	// handle validation errors
 	if (!validator.validate()) {
 		res.json({ status: "error", errors: validator.errors });
 		return;
 	}
 
+	// prepare payload and send email
 	const text = "From: " + req.body.email + "\n" + req.body.text;
 
 	mailer
@@ -221,4 +243,5 @@ api.post("/contact", async (req: any, res: any) => {
 		});
 });
 
+// export express app as /api/ function
 exports.api = functions.https.onRequest(api);
